b178957d30801cf6760e6d3241d7a720e5a8821c
unknown <fortin_85@hotmail.com>
2018-07-28 19:25:57 -0400

    added jquery to log value of search item

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,17 +1,28 @@
-var Trackster = {};
+$(document).ready(function() {
+  /*When search button is clicked*/
+  $('#search-button').click(function() {
+    var searchText = $('#search-input').val();
+    Trackster.searchTracksByTitle(searchText);
+  });
+
+
+  var Trackster = {};
+
+
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
-Trackster.renderTracks = function(tracks) {
+  Trackster.renderTracks = function(tracks) {
 
-};
+  };
 
 /*
   Given a search term as a string, query the LastFM API.
   Render the tracks given in the API query response.
 */
-Trackster.searchTracksByTitle = function(title) {
+  Trackster.searchTracksByTitle = function(title) {
 
-};
+  };
+});
