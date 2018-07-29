c9db92546527a10cdfbfea2583ca2d18e72c79bd
unknown <fortin_85@hotmail.com>
2018-07-29 17:19:40 -0400

    cleaned up comments

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -9,13 +9,6 @@ $(document).ready(function() {
   });
 
 
-/*       $.ajax({
-      url: 'https://api.wunderground.com/api/4e1fe0d302980707/geolookup/conditions/q/NY/Manhattan.json',
-      datatype: 'jsonp',
-      success: function(data) {
-        $('.weather').html('<p class="data"> </p>');
-      }
-    });
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
@@ -53,16 +46,3 @@ $(document).ready(function() {
     });
   };
 });
-
-
-/* API Info
-
-Application name	School Project
-API key	c2e05b0963a93c365395afd60103ad60
-Shared secret	79586a816e1e61792b2b6c6cd2cdcc4b
-Registered to	danfortin75
-
-test API
-
-http://ws.audioscrobbler.com/2.0/?method=track.search&track=tiny&api_key=c2e05b0963a93c365395afd60103ad60&format=json
-*/
