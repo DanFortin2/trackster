471df0ae8e5b37c61b2d254228b34d8c732d2f41
unknown <fortin_85@hotmail.com>
2018-07-28 20:45:21 -0400

    set up API call to return list of songs and styled song header titles

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -5,11 +5,18 @@ $(document).ready(function() {
     Trackster.searchTracksByTitle(searchText);
   });
 
-
+  const API_KEY = 'c2e05b0963a93c365395afd60103ad60';
   var Trackster = {};
 
 
 
+/*       $.ajax({
+      url: 'https://api.wunderground.com/api/4e1fe0d302980707/geolookup/conditions/q/NY/Manhattan.json',
+      datatype: 'jsonp',
+      success: function(data) {
+        $('.weather').html('<p class="data"> </p>');
+      }
+    });
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
@@ -23,6 +30,25 @@ $(document).ready(function() {
   Render the tracks given in the API query response.
 */
   Trackster.searchTracksByTitle = function(title) {
-
+    $.ajax({
+      url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+      datatype: 'jsonp',
+      success: function(data) {
+        console.log(data);
+      }
+    });
   };
 });
+
+
+/* API Info
+
+Application name	School Project
+API key	c2e05b0963a93c365395afd60103ad60
+Shared secret	79586a816e1e61792b2b6c6cd2cdcc4b
+Registered to	danfortin75
+
+test API
+
+http://ws.audioscrobbler.com/2.0/?method=track.search&track=tiny&api_key=c2e05b0963a93c365395afd60103ad60&format=json
+*/
